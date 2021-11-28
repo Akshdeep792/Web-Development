f9ced3a56b634ad6611a9db2952979896283232b
Akshdeep Singh <76034766+Akshdeep792@users.noreply.github.com>
2021-11-28 04:58:15 -0500

    Update template.js

--- template.js
+++ template.js
@@ -3,11 +3,17 @@ const bodyParser = require("body-parser");
 const app = express();
 require("./db/conn");
 const contact = require("./models/contact");
-const ejs = require('ejs');
+const hbs = require('hbs');
 const path = require("path");
 
-
-app.set('view engine', 'ejs');
+const templatePath = path.join(__dirname, " ../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
+app.set('view engine', 'hbs');
+app.set('views', templatePath);
+hbs.registerPartials(partialPath);
+app.use('/css', express.static(path.join(__dirname , "../node_modules/bootstrap/dist/css")));
+app.use('/js', express.static(path.join(__dirname , "../node_modules/bootstrap/dist/js")));
+app.use('/jq', express.static(path.join(__dirname , "../node_modules/jquery/dist")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
@@ -15,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.get("/", (req,res) => {
-    res.send("Hello");
+    res.render("index");
 })
 
 
