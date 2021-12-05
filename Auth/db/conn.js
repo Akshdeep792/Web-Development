afbf9db6f1e57821d8c693b24eaca402a7dc0d00
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2021-12-05 16:41:18 -0500

    Crash

--- Auth/db/conn.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/Auth")
-    .then(() => console.log("connection successfull........."))
-    .catch((err) => console.log(err));
-
-    // mongoose.set({getUserIndex:"true"});
