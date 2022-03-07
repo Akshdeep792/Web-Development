47c4aa38e5ec55031be34f1e32667e591d27befb
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2022-03-07 14:55:52 -0500

    Some changes

--- Backend/Login/src/db/conn.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/Register")
-    .then(() => console.log("connection successfull........."))
-    .catch((err) => console.log(err));
