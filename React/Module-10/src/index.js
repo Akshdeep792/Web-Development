9c6780adcd2544179999bf58e3a5be33bec173a1
Akshdeep792 <76034766+Akshdeep792@users.noreply.github.com>
2022-03-08 14:48:34 -0500

    Changes

--- React/Module-10/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-import AuthContext from './context/auth-context';
-
-ReactDOM.render(<AuthContext><App /></AuthContext>, document.getElementById('root'));
